598fb21b53db120baae051fb620d502947d69022
juanzang242426 <juanzang@dimigo.hs.kr>
2023-09-21 21:57:48 +0900

    커밋메시지

---
+++ script.js
@@ -0,0 +1,17 @@
+alert("HI");
+const hello=document.getElementById('h');
+hello.innerText='jfslkjfklsajflaj';
+hello.style.color ="yellow";
+// while(1){
+//    const input=document.getElementById('input');
+
+// }
+const button = document.getElementById("but");
+button.addEventListener('click',()=>{
+    alert('클릭 되었습니다.');
+})
+function hk(){
+    alert('tkfkdgo');
+}
+setInterval(hk,2000);
+clearInterval(hkfunction);
